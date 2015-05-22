d301c96b2a5cd8348212ae054b5937ef6128923f
LFQ <fangquan.li@monstar-lab.com>
2015-05-22 23:25:44 +0800

    no message

--- Recognize Notes/src/com/love_li2/recognizenotes/MainActivity.java
+++ Recognize Notes/src/com/love_li2/recognizenotes/MainActivity.java
@@ -1,5 +1,7 @@
 package com.love_li2.recognizenotes;
 
+import com.love_li2.recognizenotes.view.PainoKeyboard;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
@@ -17,6 +19,20 @@ public class MainActivity extends Activity {
 	private void initView() {
 		// TODO Auto-generated method stub
 		 keyboard = (LinearLayout)findViewById(R.id.main_keyborad);
-		 
+		 for (int i = 0; i < 9; i++) {
+			PainoKeyboard painoKeyboard = new PainoKeyboard(this);
+			switch (i) {
+			case 0:
+				painoKeyboard.setBeginAndEnd(5, 6);
+				break;
+			case 8:
+				painoKeyboard.setBeginAndEnd(0, 0);
+				break;
+			default:
+				painoKeyboard.setBeginAndEnd(0, 6);
+				break;
+			}
+			keyboard.addView(painoKeyboard);
+		}
 	}
 }
--- Recognize Notes/src/com/love_li2/recognizenotes/view/PainoKeyboard.java
+++ Recognize Notes/src/com/love_li2/recognizenotes/view/PainoKeyboard.java
@@ -15,12 +15,12 @@ import android.widget.Toast;
 
 public class PainoKeyboard extends LinearLayout implements OnClickListener {
 	String TAG = "PainoKeyboard";
-	
-	private int whiteKeyWidth = 0,blackeyWidth = 0, keyHeight = 0,keyNums = 16;
+
+	private int whiteKeyWidth = 0, blackeyWidth = 0, keyHeight = 0, keyNums = 16;
 	Button[] whiteKeys = new Button[7];
 	Button[] blackKeys = new Button[5];
-	int beginIndex = 0,endIndex = 6;
-	
+	int beginIndex = 0, endIndex = 6;
+
 	public PainoKeyboard(Context context, AttributeSet attrs, int defStyle) {
 		super(context, attrs, defStyle);
 		init(context);
@@ -36,18 +36,26 @@ public class PainoKeyboard extends LinearLayout implements OnClickListener {
 		init(context);
 	}
 
+	public void setBeginAndEnd(int b, int e) {
+		beginIndex = b;
+		endIndex = e;
+		if (beginIndex == 5) {
+			blackKeys[3].setVisibility(View.GONE);
+		} else if (endIndex == 0) {
+			blackKeys[0].setVisibility(View.GONE);
+		}
+	}
+
 	private void init(Context context) {
 		setOrientation(LinearLayout.HORIZONTAL);
-		WindowManager wm = (WindowManager) getContext()
-                .getSystemService(Context.WINDOW_SERVICE);
+		WindowManager wm = (WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE);
 		Point outSize = new Point();
-		wm.getDefaultDisplay().getSize(outSize );
+		wm.getDefaultDisplay().getSize(outSize);
 		whiteKeyWidth = outSize.x / keyNums;
 		blackeyWidth = whiteKeyWidth * 5 / 7;
 		keyHeight = whiteKeyWidth * 5;
-		
-		for(int i =0 ;i < 7 ; i++)
-		{
+
+		for (int i = 0; i < 7; i++) {
 			whiteKeys[i] = new Button(context);
 			whiteKeys[i].setId(i);
 			whiteKeys[i].setPadding(0, 0, 0, 0);
@@ -55,43 +63,36 @@ public class PainoKeyboard extends LinearLayout implements OnClickListener {
 			whiteKeys[i].setBackgroundResource(R.drawable.key_background);
 			addView(whiteKeys[i]);
 		}
-		for(int i =0 ;i < 5 ; i++)
-		{
+		for (int i = 0; i < 5; i++) {
 			blackKeys[i] = new Button(context);
-			blackKeys[i].setId(i+7);
+			blackKeys[i].setId(i + 7);
 			blackKeys[i].setPadding(0, 0, 0, 0);
 			blackKeys[i].setOnClickListener(this);
 			blackKeys[i].setBackgroundColor(Color.BLACK);
 			addView(blackKeys[i]);
 		}
 	}
-	
+
 	@Override
 	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
 		// TODO Auto-generated method stub
 		setMeasuredDimension(whiteKeyWidth * (endIndex - beginIndex + 1), keyHeight);
 	}
-	
+
 	@Override
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
 		// TODO Auto-generated method stub
-		for(int i = 0 ;i < 7 ; i++)
-		{
-			whiteKeys[i].layout((i-beginIndex)*whiteKeyWidth, 0, (i-beginIndex+1) * whiteKeyWidth, keyHeight);
+		for (int i = 0; i < 7; i++) {
+			whiteKeys[i].layout((i - beginIndex) * whiteKeyWidth, 0, (i - beginIndex + 1) * whiteKeyWidth, keyHeight);
 		}
-		blackKeys[0].layout((int)(whiteKeyWidth * (0.5-beginIndex)), 0, (int)(whiteKeyWidth * (0.5 - beginIndex)) + blackeyWidth, keyHeight/3*2);
-		blackKeys[1].layout((int)(whiteKeyWidth * (1 + 5.0/7)), 0, (int)(whiteKeyWidth * (1 + 5.0/7 + 5.0/7)), keyHeight/3*2);
-		blackKeys[2].layout((int)(whiteKeyWidth * 3.5), 0, (int)(whiteKeyWidth * 4.2), keyHeight/3*2);
-		blackKeys[3].layout((int)(whiteKeyWidth * 4.5), 0, (int)(whiteKeyWidth * 5.2), keyHeight/3*2);
-		blackKeys[4].layout((int)(whiteKeyWidth * 5.5), 0, (int)(whiteKeyWidth * 6.2), keyHeight/3*2);
+		blackKeys[0].layout((int) (whiteKeyWidth * (0.5 - beginIndex)), 0, (int) (whiteKeyWidth * (0.5 - beginIndex)) + blackeyWidth, keyHeight / 3 * 2);
+		blackKeys[1].layout((int) (whiteKeyWidth * (1.7 - beginIndex)), 0, (int) (whiteKeyWidth * (1.7 - beginIndex)) + blackeyWidth, keyHeight / 3 * 2);
+		blackKeys[2].layout((int) (whiteKeyWidth * (3.45 - beginIndex)), 0, (int) (whiteKeyWidth * (3.45 - beginIndex)) + blackeyWidth, keyHeight / 3 * 2);
+		blackKeys[3].layout((int) (whiteKeyWidth * (4.65 - beginIndex)), 0, (int) (whiteKeyWidth * (4.65 - beginIndex)) + blackeyWidth, keyHeight / 3 * 2);
+		blackKeys[4].layout((int) (whiteKeyWidth * (5.85 - beginIndex)), 0, (int) (whiteKeyWidth * (5.85 - beginIndex)) + blackeyWidth, keyHeight / 3 * 2);
 	}
 
 	@Override
 	public void onClick(View v) {
-		Toast.makeText(getContext(), ""+v.getId(), 0).show();
-	}
-	
-	private class NullKeyboardException extends Exception{
-		
 	}
 }
